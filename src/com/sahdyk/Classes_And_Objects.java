2ee2c52d8bdede820013514ed3338254bcb2594b
sahdyk <sahdk209@gmail.com>
2020-09-02 01:26:14 -0700

    OOP Tutorial

---
+++ src/com/sahdyk/Classes_And_Objects.java
@@ -0,0 +1,23 @@
+package com.sahdyk;
+// #1
+public class Classes_And_Objects {
+/*
+        Classes: Blueprints for creating objects
+        Objects: The instance(creation) of the class.
+
+        Classes consist of two things:
+                FIELDS - Conditions that the object will have.
+                for example: A tv will have
+                currentVolume
+                CurrentChannel
+                isOn
+                and
+                METHODS - are basically verbs or actions that change the conditions.
+                For example: to change the tv conditions
+                turnOff();
+                turnOn();
+                increaseVolume();
+                decreaseVolume();
+
+ */
+}
